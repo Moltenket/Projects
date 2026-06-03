55531f0838dcf1d945d394267abd90fed5181f46
Moltenket <101940488+Moltenket@users.noreply.github.com>
2026-06-03 11:35:55 -0300

    Add files via upload

--- PjBL2-BatalhaNaval/BatalhaNaval.py
+++ PjBL2-BatalhaNaval/BatalhaNaval.py
@@ -3,19 +3,23 @@ from rich import print
 #valores
 l = 0
 c = 0
-marca_X = "[bright_red]X[/bright_red]"
 z = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+marca_X = "[bright_red]X[/bright_red]"
 
 #cria matrizes
-tamanho_matriz = int(input("Qual o tamanho da matriz desejado?: (min:10x10, max:26x26)"))
-matrizA = [["~"] * tamanho_matriz for _ in range(tamanho_matriz)]
+tamanho_matriz = input("Qual o tamanho da matriz desejado?: (min:10x10, max:26x26)")
+novotamanho = tamanho_matriz.split("x")
+largura_matriz = int(novotamanho[0])
+coluna_matriz = int(novotamanho[1])
+
+matrizA = [["~"] * largura_matriz for _ in range(coluna_matriz)]
 matrizN = []
 for i in range(len(matrizA)):
   matrizN.append(z[i])
 
 def printA():
   #print(f"0 {matrizN}")
-  print("[bright_white]-   0    1    2    3    4    5    6    7    8    9[/bright_white]")
+  print("[bright_white]-\t0\t1\t2\t3\t4\t5\t6\t7\t8\t9[/bright_white]")
   for i in range(len(matrizA)):
     print(z[i],f"[turquoise2]{matrizA[i]}[/turquoise2]")
   print()
